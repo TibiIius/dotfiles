d32e27fbc10e4574937b06ec4db8ec4120a86447
TimFB <56914732+TibiIius@users.noreply.github.com>
2020-06-14 21:53:21 +0200

    refactor

--- i3-setup/.profile
+++
@@ -1,4 +0,0 @@
-export EDITOR=/usr/bin/nvim
-# fix "xdg-open fork-bomb" export your preferred browser from here
-export BROWSER=/usr/bin/firefox
-export TERMINAL=/usr/bin/tilix
--- suckless/dwm-6.2/config.h
+++ suckless/dwm-6.2/config.h
@@ -6,8 +6,8 @@ static const unsigned int gappx     = 5;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=12" };
+static const char dmenufont[]       = "JetBrains Mono Nerd Font:size=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -22,6 +22,9 @@ static const char *colors[][3]      = {
 static const char *const autostart[] = {
 	"sh", "-c", "/home/tim/Documents/GitHub/dotfiles/scripts/screenlayout.sh", NULL,
 	"nitrogen", "--restore", NULL,
+	"picom", NULL,
+	"keepass", NULL,
+	"discord", NULL,
 	NULL /* terminate */
 };
 
