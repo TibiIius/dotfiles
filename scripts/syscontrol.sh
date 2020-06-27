b60beda96f49dcf0089313fc0d2709df2a5a2062
TimFB <56914732+TibiIius@users.noreply.github.com>
2020-06-27 16:48:14 +0200

    fixed shit

--- scripts/syscontrol.sh
+++ scripts/syscontrol.sh
@@ -3,7 +3,7 @@
 MENU="$(rofi -sep "|" -dmenu -i -p 'What do you want to do?' <<< " Lock| Reboot| Shutdown")"
 
 case "$MENU" in
-	Lock) slock ;;
-	Reboot) reboot ;;
-	Shutdown) poweroff ;;
+	*Lock) slock ;;
+	*Reboot) reboot ;;
+	*Shutdown) poweroff ;;
 esac
