80d058f7dcb87b2cc14f4e0ee185f71f00091dbf
TimFB <56914732+TibiIius@users.noreply.github.com>
2020-06-28 22:17:44 +0200

    logout function added

--- scripts/syscontrol.sh
+++ scripts/syscontrol.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-MENU="$(rofi -sep "|" -dmenu -i -p 'What do you want to do?' <<< " Lock| Reboot| Shutdown")"
+MENU="$(rofi -sep "|" -dmenu -i -p 'What do you want to do?' <<< " Logout| Lock| Reboot| Shutdown")"
+dwm=$( ps -o pid,cmd ax | awk '/dwm/{ if ($2 == "dwm") print $1 }' )
 
 case "$MENU" in
+	*Logout) [[ ! -z "$dwm" ]] && kill -s TERM $dwm ;;
 	*Lock) slock ;;
 	*Reboot) reboot ;;
 	*Shutdown) shutdown -h now ;;
