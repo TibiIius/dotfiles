9d39694bb701e8fcb8b8a4d815c5111e999ae6fb
TimFB <56914732+TibiIius@users.noreply.github.com>
2020-07-02 20:11:31 +0200

    removed appointment notification (now handled by calcurse daemon)

--- scripts/appointments.sh
+++ scripts/appointments.sh
@@ -9,14 +9,14 @@ APPOINTMENTS_TODAY=$(calcurse -a)
 if [ -n "$APPOINTMENTS_TODAY" ]; then
 	notify-send "$(calcurse -a)"
 else
-	notify-send "No Appointments Today" "Be free! :D"
+	notify-send "No Appointments Today"
 fi
 	
 while true; do
 	calcurse-caldav > /dev/null 2>&1
-	APPOINTMENT=$(calcurse -n | awk -F '[][]'  '/\[/ { print $2 }')
-	if [ "$APPOINTMENT" == '01:00' ] || [ "$APPOINTMENT" == '00:59' ]; then
-	notify-send "Next Appointment: $(calcurse -n | awk '/\]/ { print $2 }')" "in an hour."
-	fi
-	sleep 60
+#	APPOINTMENT=$(calcurse -n | awk -F '[][]'  '/\[/ { print $2 }')
+#	if [ "$APPOINTMENT" == '01:00' ] || [ "$APPOINTMENT" == '00:59' ]; then
+#	notify-send "Next Appointment: $(calcurse -n | awk '/\]/ { print $2 }')" "in an hour."
+#	fi
+	sleep 120
 done;
