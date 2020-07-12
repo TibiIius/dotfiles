fab9f2bffb5c8c6fe1d269245943c0d70377ba71
TimFB <56914732+TibiIius@users.noreply.github.com>
2020-07-12 19:10:15 +0200

    minor tweaks

--- suckless/dwm-6.2/config.h
+++ suckless/dwm-6.2/config.h
@@ -6,10 +6,10 @@
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappih    = 24;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 24;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 24;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 24;       /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayspacing = 2;   /* systray spacing */
@@ -127,20 +127,20 @@ static Key keys[] = {
 	{ MODKEY,                       XK_p,                    incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,                    setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,                    setmfact,       {.f = +0.05} },
-	{ MODKEY|Mod4Mask,              XK_h,                    incrgaps,       {.i = +1 } },
-	{ MODKEY|Mod4Mask,              XK_l,                    incrgaps,       {.i = -1 } },
-	{ MODKEY|Mod4Mask|ShiftMask,    XK_h,                    incrogaps,      {.i = +1 } },
-	{ MODKEY|Mod4Mask|ShiftMask,    XK_l,                    incrogaps,      {.i = -1 } },
-	{ MODKEY|Mod4Mask|ControlMask,  XK_h,                    incrigaps,      {.i = +1 } },
-	{ MODKEY|Mod4Mask|ControlMask,  XK_l,                    incrigaps,      {.i = -1 } },
-	{ MODKEY|Mod4Mask,              XK_0,                    togglegaps,     {0} },
-	{ MODKEY|Mod4Mask|ShiftMask,    XK_0,                    defaultgaps,    {0} },
+	{ MODKEY|Mod1Mask,              XK_h,                    incrgaps,       {.i = +1 } },
+	{ MODKEY|Mod1Mask,              XK_l,                    incrgaps,       {.i = -1 } },
+	{ MODKEY|Mod1Mask|ShiftMask,    XK_h,                    incrogaps,      {.i = +1 } },
+	{ MODKEY|Mod1Mask|ShiftMask,    XK_l,                    incrogaps,      {.i = -1 } },
+	{ MODKEY|Mod1Mask|ControlMask,  XK_h,                    incrigaps,      {.i = +1 } },
+	{ MODKEY|Mod1Mask|ControlMask,  XK_l,                    incrigaps,      {.i = -1 } },
+	{ MODKEY|Mod1Mask,              XK_0,                    togglegaps,     {0} },
+	{ MODKEY|Mod1Mask|ShiftMask,    XK_0,                    defaultgaps,    {0} },
 	{ MODKEY,                       XK_y,                    incrihgaps,     {.i = +1 } },
 	{ MODKEY,                       XK_o,                    incrihgaps,     {.i = -1 } },
 	{ MODKEY|ControlMask,           XK_y,                    incrivgaps,     {.i = +1 } },
 	{ MODKEY|ControlMask,           XK_o,                    incrivgaps,     {.i = -1 } },
-	{ MODKEY|Mod4Mask,              XK_y,                    incrohgaps,     {.i = +1 } },
-	{ MODKEY|Mod4Mask,              XK_o,                    incrohgaps,     {.i = -1 } },
+	{ MODKEY|Mod1Mask,              XK_y,                    incrohgaps,     {.i = +1 } },
+	{ MODKEY|Mod1Mask,              XK_o,                    incrohgaps,     {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_y,                    incrovgaps,     {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_o,                    incrovgaps,     {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_Return,               zoom,           {0} },
