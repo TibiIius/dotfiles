168600342a16484e680a0c311e0404e84cbee6a5
TimFB <56914732+TibiIius@users.noreply.github.com>
2020-07-12 18:04:57 +0200

    added text feedback

--- scripts/scrot.sh
+++ scripts/scrot.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
+notify-send 'Select area to screenshot'
 sleep 0.1
-scrot -s /home/tim/Documents/Screenshots/%d-%m-%Y-%T-scrot.png
+scrot -s /home/tim/Documents/Screenshots/%d-%m-%Y-%T-scrot.png && notify-send 'Screenshot taken' 'saved in ~/Documents/Screenshots'
