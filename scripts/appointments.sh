b7940cc074febe563953a6949d94d55880e4cad6
TimFB <56914732+TibiIius@users.noreply.github.com>
2020-07-12 18:04:24 +0200

    restored reminder

--- scripts/appointments.sh
+++ scripts/appointments.sh
@@ -14,9 +14,9 @@ fi
 	
 while true; do
 	calcurse-caldav > /dev/null 2>&1
-#	APPOINTMENT=$(calcurse -n | awk -F '[][]'  '/\[/ { print $2 }')
-#	if [ "$APPOINTMENT" == '01:00' ] || [ "$APPOINTMENT" == '00:59' ]; then
-#	notify-send "Next Appointment: $(calcurse -n | awk '/\]/ { print $2 }')" "in an hour."
-#	fi
+	APPOINTMENT=$(calcurse -n | awk -F '[][]'  '/\[/ { print $2 }')
+	if [ "$APPOINTMENT" == '01:00' ] || [ "$APPOINTMENT" == '00:59' ]; then
+		notify-send "Next Appointment: $(calcurse -n | awk '/\]/ { print $2 }')" "in an hour."
+	fi
 	sleep 120
 done;
