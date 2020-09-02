994ec8d394d1f1896a5c680fc25fc210ef3989df
TimFB <56914732+TibiIius@users.noreply.github.com>
2020-09-02 21:08:47 +0200

    scrot.sh tweak

--- scripts/scrot.sh
+++ scripts/scrot.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
-notify-send 'Select area to screenshot'
-sleep 0.1
-scrot -s /home/tim/Documents/Screenshots/%d-%m-%Y-%T-scrot.png && notify-send 'Screenshot taken' 'saved in ~/Documents/Screenshots'
+if [[ $1 == "area" ]]; then
+	notify-send 'Select area to screenshot'
+	sleep 0.1
+	scrot -s /home/tim/Documents/Screenshots/%d-%m-%Y-%T-scrot.png && notify-send 'Screenshot taken' 'saved in ~/Documents/Screenshots'
+elif [[ $1 == "focused" ]]; then
+	notify-send 'Focused window will be screenshotted shortly'
+	sleep 0.1
+	scrot -u /home/tim/Documents/Screenshots/%d-%m-%Y-%T-scrot.png && notify-send 'Screenshot taken' 'saved in ~/Documents/Screenshots'
+fi
