f50e3a258378d1a51c44c60bf72fb7e9edcf24c6
TimFB <56914732+TibiIius@users.noreply.github.com>
2020-10-26 18:26:25 +0100

    arrow keys for movement between monitors

--- suckless/dwm-6.2/config.h
+++ suckless/dwm-6.2/config.h
@@ -169,10 +169,10 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_space,                togglefloating, {0} },
 	{ MODKEY,                       XK_0,                    view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,                    tag,            {.ui = ~0 } },
-	{ MODKEY,                       XK_comma,                focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_period,               focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,                tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period,               tagmon,         {.i = +1 } },
+	{ MODKEY,                       XK_Left,                 focusmon,       {.i = -1 } },
+	{ MODKEY,                       XK_Right,                focusmon,       {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_Left,                 tagmon,         {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_Right,                tagmon,         {.i = +1 } },
 	{ MODKEY,                       XK_F5,                   xrdb,           {.v = NULL } },
 	TAGKEYS(                        XK_1,                                    0)
 	TAGKEYS(                        XK_2,                                    1)
