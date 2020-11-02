5e6bbe83384f1fe06cb751e7bfa2d29ce2e92eac
TimFB <56914732+TibiIius@users.noreply.github.com>
2020-11-02 12:00:58 +0100

    commented out appointent reminder for now

--- scripts/appointments.sh
+++ scripts/appointments.sh
@@ -12,11 +12,11 @@ else
 	notify-send "No appointments today"
 fi
 	
-while true; do
-	calcurse-caldav > /dev/null 2>&1
-	APPOINTMENT=$(calcurse -n | awk -F '[][]'  '/\[/ { print $2 }')
-	if [ "$APPOINTMENT" == '01:00' ] || [ "$APPOINTMENT" == '00:59' ]; then
-		notify-send "Next appointment: $(calcurse -n | awk '/\]/ { print $2 }')" "in an hour."
-	fi
-	sleep 120
-done;
+#while true; do
+#	calcurse-caldav > /dev/null 2>&1
+#	APPOINTMENT=$(calcurse -n | awk -F '[][]'  '/\[/ { print $2 }')
+#	if [ "$APPOINTMENT" == '01:00' ] || [ "$APPOINTMENT" == '00:59' ]; then
+#		notify-send "Next appointment: $(calcurse -n | awk '/\]/ { print $2 }')" "in an hour."
+#	fi
+#	sleep 120
+#done;
