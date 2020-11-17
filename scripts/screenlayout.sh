b198df17fed6b76495cb2799c698c44be5261b9c
TimFB <56914732+TibiIius@users.noreply.github.com>
2020-11-17 20:41:00 +0100

    Changed screenlayout due to device name changes

--- scripts/screenlayout.sh
+++ scripts/screenlayout.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DisplayPort-0 --off --output DisplayPort-1 --off --output HDMI-0 --mode 1920x1080 --pos 0x0 --rotate normal --output DVI-0 --primary --mode 1920x1080 --rate 144 --pos 1920x0 --rotate normal --output DVI-1 --off
+xrandr --output DisplayPort-0 --off --output DisplayPort-1 --off --output HDMI-A-0 --mode 1920x1080 --pos 0x0 --rotate normal --output DVI-I-0 --primary --mode 1920x1080 --rate 144 --pos 1920x0 --rotate normal --output DVI-D-1 --off
