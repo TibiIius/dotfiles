be872fa6d58ba04cce1263f930e86477dad26d09
TimFB <56914732+TibiIius@users.noreply.github.com>
2020-11-25 16:58:19 +0100

    general fixes + tabbar always on and at bottom

--- suckless/dwm-6.2/config.h
+++ suckless/dwm-6.2/config.h
@@ -11,18 +11,18 @@ static const unsigned int gappiv         = 20;       /* vert inner gap between w
 static const unsigned int gappoh         = 20;       /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov         = 20;       /* vert outer gap between windows and screen edge */
 static const int smartgaps               = 0;        /* 1 means no outer gap when there is only one window */
-static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
-static const unsigned int systrayspacing = 2;   /* systray spacing */
-static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
-static const int showsystray             = 1;     /* 0 means no systray */
+static const unsigned int systraypinning = 0;        /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayspacing = 2;        /* systray spacing */
+static const int systraypinningfailfirst = 1;        /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const int showsystray             = 1;        /* 0 means no systray */
 static const int showbar                 = 1;        /* 0 means no bar */
 static const int topbar                  = 1;        /* 0 means bottom bar */
 /*  Display modes of the tab bar: never shown, always shown, shown only in  */
 /*  monocle mode in the presence of several windows.                        */
 /*  Modes after showtab_nmodes are disabled.                                */
 enum showtab_modes { showtab_never, showtab_auto, showtab_nmodes, showtab_always};
-static const int showtab                 = showtab_auto;        /* Default tab bar show mode */
-static const int toptab                  = True;               /* False means bottom tab bar */
+static const int showtab                 = showtab_always;      /* Default tab bar show mode */
+static const int toptab                  = False;               /* False means bottom tab bar */
 static const char *fonts[]               = { "JetBrainsMono Nerd Font:size=10" };
 static const char dmenufont[]            = "JetBrainsMono Nerd Font:size=10";
 static char normbgcolor[]                = "#222222";
@@ -75,11 +75,13 @@ static const Rule rules[] = {
 	/* class     	          instance    title     														  	tags mask     isfloating   monitor */
 	{ "Gimp",               NULL,       NULL,                                     0,            1,           -1 },
 /*{ "Firefox",            NULL,       NULL,                                     1 << 8,       0,           -1 }, */
-	{ "KeePassXC",          NULL,       NULL,                                     1 << 8,       0,           -1 },
+	{ "KeePassXC",          NULL,       NULL,                                     1 << 8,       0,            0 },
 	{ "discord",            NULL,       NULL,                                     1 << 7,       0,            1 },
-	{ NULL,                 NULL,       "Org.gnome.DejaDup",                      1 << 8,       0,            1 },
+	{ NULL,                 NULL,       "Backups",                                1 << 8,       0,            1 },
+	{ "Geary",              NULL,       NULL,                                     1 << 7,       0,            0 },
 	{ "Steam",              NULL,       NULL,    								                  1 << 6,       1,            1 },
-	{ "Spotify",            NULL,       NULL,                                     1 << 6,       0,           -1 },
+	{ "Spotify",            NULL,       NULL,                                     1 << 6,       0,            0 },
+	{ NULL,                 NULL,       "Calculator",                             0,            1,           -1 },
 };
 
 /* layout(s) */
