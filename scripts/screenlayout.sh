b6b8e201d27b73859e43dae7305647fff00e4cf5
TimFB <56914732+TibiIius@users.noreply.github.com>
2020-12-14 12:31:18 +0100

    scripts: changed screenlayout to match new monitor setup

--- scripts/screenlayout.sh
+++ scripts/screenlayout.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DisplayPort-0 --off --output DisplayPort-1 --primary --mode 1920x1080 --rate 144 --pos 1920x0 --rotate normal --output HDMI-A-0 --off --output HDMI-A-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DVI-D-0 --mode 1280x1024 --pos 3840x0 --rotate normal
+xrandr --output DisplayPort-0 --off --output DisplayPort-1 --primary --mode 1920x1080 --rate 144 --pos 1920x0 --rotate normal --output HDMI-A-0 --off --output HDMI-A-1 --mode 1920x1080 --pos 0x0 --left-of DisplayPort-1 --rotate normal --output DVI-D-0 --mode 1280x1024 --pos 3840x0 --rate 75 --right-of DisplayPort-1 --rotate normal
