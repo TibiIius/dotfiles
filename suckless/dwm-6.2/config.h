c29da3f4a79a92ed237d9d97a5e71abc092a8ff9
TimFB <56914732+TibiIius@users.noreply.github.com>
2020-12-26 18:37:24 +0100

    change border size to 1px

--- suckless/dwm-6.2/config.h
+++ suckless/dwm-6.2/config.h
@@ -4,7 +4,7 @@
 #include <X11/XF86keysym.h>
 
 /* appearance */
-static const unsigned int borderpx       = 2;        /* border pixel of windows */
+static const unsigned int borderpx       = 1;        /* border pixel of windows */
 static const unsigned int snap           = 32;       /* snap pixel */
 static const unsigned int gappih         = 20;       /* horiz inner gap between windows */
 static const unsigned int gappiv         = 20;       /* vert inner gap between windows */
