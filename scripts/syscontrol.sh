6828d375faabe59dbd6b76cfd3b0efdaac45fde6
TimFB <56914732+TibiIius@users.noreply.github.com>
2021-01-07 10:50:23 +0100

    using slock instead of light-locker

--- scripts/syscontrol.sh
+++ scripts/syscontrol.sh
@@ -5,7 +5,7 @@ dwm=$( ps -o pid,cmd ax | awk '/dwm/{ if ($2 == "dwm") print $1 }' )
 
 case "$MENU" in
 	*Logout) [[ ! -z "$dwm" ]] && kill -s TERM $dwm ;;
-	*Lock) light-locker-command -l ;;
+	*Lock) slock ;;
 	*Reboot) reboot ;;
 	*Shutdown) shutdown -h now ;;
 esac
