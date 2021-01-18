e0910ea5da1683fad687a5d50c9fa7e152aae47c
TimFB <56914732+TibiIius@users.noreply.github.com>
2021-01-18 14:28:32 +0100

    GOT MY 3080 LETS GOOOOOO

--- scripts/screenlayout.sh
+++ scripts/screenlayout.sh
@@ -1,11 +1,2 @@
 #!/bin/sh
-
-MONITOR_HDMI=$(xrandr --listmonitors | grep HDMI-A-1)
-MONITOR_DVI=$(xrandr --listmonitors | grep DVI-D-0)
-
-if [ -n "$MONITOR_DVI" ] && [ -n "$MONITOR_HDMI" ]; then
-  xrandr --output DisplayPort-0 --off --output DisplayPort-1 --primary --mode 1920x1080 --rate 144 --pos 1920x0 --rotate normal --output HDMI-A-0 --off --output HDMI-A-1 --mode 1920x1080 --pos 0x0 --left-of DisplayPort-1 --rotate normal --output DVI-D-0 --mode 1280x1024 --pos 3840x0 --rate 75 --right-of DisplayPort-1 --rotate normal
-else
-  xrandr --output DisplayPort-0 --off --output DisplayPort-1 --primary --mode 1920x1080 --rate 144 --pos 0x0 --rotate normal --output HDMI-A-0 --off --output HDMI-A-1 --off --output DVI-D-0 --off
-fi
-
+xrandr --output DP-0 --off --output DP-1 --off --output HDMI-0 --off --output DP-2 --off --output DP-3 --off --output HDMI-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-4 --mode 1920x1080 --primary --rate 144 --pos 1920x0 --rotate normal --output DP-5 --off
