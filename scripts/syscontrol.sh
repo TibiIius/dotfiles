34b1be9142738c1d15dee69dcc29fe72633b5e3a
TimFB <56914732+TibiIius@users.noreply.github.com>
2021-02-22 14:48:16 +0100

    syscontrol: change to i3lock-color

--- scripts/syscontrol.sh
+++ scripts/syscontrol.sh
@@ -5,7 +5,7 @@ dwm=$( ps -o pid,cmd ax | awk '/dwm/{ if ($2 == "dwm") print $1 }' )
 
 case "$MENU" in
 	*Logout) [[ ! -z "$dwm" ]] && kill -s TERM $dwm ;;
-	*Lock) slock ;;
+	*Lock) i3lock -B sigma --indicator -k --insidecolor=a3be8c44 --insidevercolor=88c0d0aa --insidewrongcolor=fe4151ff --ringvercolor=88C0D0aa --ringcolor=a3be8caa --ringwrongcolor=fe4151ff --keyhlcolor=a3be8cff --bshlcolor=fe4151ff --wrongtext="BAKA\!" --pass-media-keys --pass-volume-keys ;;
 	*Reboot) reboot ;;
 	*Shutdown) shutdown -h now ;;
 esac
