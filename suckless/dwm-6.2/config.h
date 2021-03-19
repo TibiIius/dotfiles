85b13de9478f43d4fb2e2514232da65c4d2fbf09
TimFB <56914732+TibiIius@users.noreply.github.com>
2021-03-19 18:46:51 +0100

    dwm: change gaps

--- suckless/dwm-6.2/config.h
+++ suckless/dwm-6.2/config.h
@@ -6,10 +6,10 @@
 /* appearance */
 static const unsigned int borderpx       = 2;        /* border pixel of windows */
 static const unsigned int snap           = 32;       /* snap pixel */
-static const unsigned int gappih         = 5;        /* horiz inner gap between windows */
-static const unsigned int gappiv         = 5;        /* vert inner gap between windows */
-static const unsigned int gappoh         = 5;        /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov         = 5;        /* vert outer gap between windows and screen edge */
+static const unsigned int gappih         = 10;        /* horiz inner gap between windows */
+static const unsigned int gappiv         = 10;        /* vert inner gap between windows */
+static const unsigned int gappoh         = 20;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov         = 20;       /* vert outer gap between windows and screen edge */
 static const int smartgaps               = 0;        /* 1 means no outer gap when there is only one window */
 static const unsigned int systraypinning = 1;        /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayspacing = 2;        /* systray spacing */
