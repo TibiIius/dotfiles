7ce9d93fc87b976c43d3aedaeb51e372e0e592b1
TimFB <56914732+TibiIius@users.noreply.github.com>
2021-03-19 18:29:09 +0100

    glava: change theme a bit :)

--- glava/themes/material-ocean.bars.glsl
+++ glava/themes/material-ocean.bars.glsl
@@ -2,13 +2,13 @@
 /* Center line thickness (pixels) */
 #define C_LINE 1
 /* Width (in pixels) of each bar */
-#define BAR_WIDTH 4
+#define BAR_WIDTH 6
 /* Width (in pixels) of each bar gap */
-#define BAR_GAP 2
+#define BAR_GAP 4
 /* Outline color */
 #define BAR_OUTLINE #262626
 /* Outline width (in pixels, set to 0 to disable outline drawing) */
-#define BAR_OUTLINE_WIDTH 0
+#define BAR_OUTLINE_WIDTH 1
 /* Amplify magnitude of the results each bar displays */
 #define AMPLIFY 300
 /* Whether the current settings use the alpha channel;
@@ -20,14 +20,13 @@
 /* Bar color changes with height */
 #define GRADIENT (d / GRADIENT_POWER + 1)
 /* Bar color */
-/* define COLOR (#3366b2 * GRADIENT) */
-#define COLOR (#ff4151 * GRADIENT)
+#define COLOR #fe4151
 /* Direction that the bars are facing, 0 for inward, 1 for outward */
 #define DIRECTION 0
 /* Whether to switch left/right audio buffers */
 #define INVERT 0
 /* Whether to flip the output vertically */
-#define FLIP 0
+#define FLIP 1
 /* Whether to mirror output along `Y = X`, causing output to render on the left side of the window */
 /* Use with `FLIP 1` to render on the right side */
 #define MIRROR_YX 0
