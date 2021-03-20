74954ff6f9e6b5a96f6fc1930b68351c6de3265a
TimFB <56914732+TibiIius@users.noreply.github.com>
2021-03-20 02:22:39 +0100

    st: add back alpha

--- suckless/st-0.8.3/config.h
+++ suckless/st-0.8.3/config.h
@@ -85,7 +85,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 1.00;
+float alpha = 0.90;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
