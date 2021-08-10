4c4ca0258c9f3a46faf835071bad1c5106b49cc1
TimFB <56914732+TibiIius@users.noreply.github.com>
2021-08-11 01:15:13 +0200

    config/tmux/base16-twilight.tmux.conf: switch mouse support off
    scripts/change-theme.sh: add gtk 3.0 and 4.0

--- scripts/change-theme.sh
+++ scripts/change-theme.sh
@@ -2,10 +2,15 @@
 
 dark_theme() {
   sed -i "s/let g:material_theme_style = 'lighter'/let g:material_theme_style = 'darker'/g" /home/tim/.config/nvim/init.vim
+  sed -i "s/gtk-application-prefer-dark-theme=0/gtk-application-prefer-dark-theme=1/g" /home/tim/.config/gtk-3.0/settings.ini
+  sed -i "s/gtk-application-prefer-dark-theme=0/gtk-application-prefer-dark-theme=1/g" /home/tim/.config/gtk-4.0/settings.ini
+
 }
 
 light_theme() {
   sed -i "s/let g:material_theme_style = 'darker'/let g:material_theme_style = 'lighter'/g" /home/tim/.config/nvim/init.vim
+  sed -i "s/gtk-application-prefer-dark-theme=1/gtk-application-prefer-dark-theme=0/g" /home/tim/.config/gtk-3.0/settings.ini
+  sed -i "s/gtk-application-prefer-dark-theme=1/gtk-application-prefer-dark-theme=0/g" /home/tim/.config/gtk-4.0/settings.ini
 }
 
 case $1 in
