93012c145f28655e763863107798f5ab918b7113
TimFB <56914732+TibiIius@users.noreply.github.com>
2021-11-24 00:39:08 +0100

    scripts: change-theme now also changes flatpak vscode

--- scripts/change-theme.sh
+++ scripts/change-theme.sh
@@ -5,6 +5,7 @@ dark_theme() {
   sed -i "s/gtk-application-prefer-dark-theme=0/gtk-application-prefer-dark-theme=1/g" /home/tim/.config/gtk-3.0/settings.ini
   sed -i "s/gtk-application-prefer-dark-theme=0/gtk-application-prefer-dark-theme=1/g" /home/tim/.config/gtk-4.0/settings.ini
   sed -i "s/\"workbench.colorTheme\": \"Material Theme Lighter\",/\"workbench.colorTheme\": \"Horizon\",/g" /home/tim/.config/Code/User/settings.json
+  sed -i "s/\"workbench.colorTheme\": \"Material Theme Lighter\",/\"workbench.colorTheme\": \"Horizon\",/g" /home/tim/.var/app/com.visualstudio.code/config/Code/User/settings.json
   [[ -f /home/tim/.config/spicetify/config-xpui.ini ]] && sed -i "s/color_scheme            = light/color_scheme            = dark/g" /home/tim/.config/spicetify/config-xpui.ini && spicetify -qn apply enable-devtool
 }
 
@@ -13,6 +14,7 @@ light_theme() {
   sed -i "s/gtk-application-prefer-dark-theme=1/gtk-application-prefer-dark-theme=0/g" /home/tim/.config/gtk-3.0/settings.ini
   sed -i "s/gtk-application-prefer-dark-theme=1/gtk-application-prefer-dark-theme=0/g" /home/tim/.config/gtk-4.0/settings.ini
   sed -i "s/\"workbench.colorTheme\": \"Horizon\",/\"workbench.colorTheme\": \"Material Theme Lighter\",/g" /home/tim/.config/Code/User/settings.json
+  sed -i "s/\"workbench.colorTheme\": \"Horizon\",/\"workbench.colorTheme\": \"Material Theme Lighter\",/g" /home/tim/.var/app/com.visualstudio.code/config/Code/User/settings.json
   [[ -f /home/tim/.config/spicetify/config-xpui.ini ]] && sed -i "s/color_scheme            = dark/color_scheme            = light/g" /home/tim/.config/spicetify/config-xpui.ini && spicetify -qn apply enable-devtool
 }
 
