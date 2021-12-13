a47f5a363016f4d9bd2f83987de8d566cad291ef
TimFB <56914732+TibiIius@users.noreply.github.com>
2021-12-13 20:19:16 +0100

    change vscode theme

--- scripts/change-theme.sh
+++ scripts/change-theme.sh
@@ -4,8 +4,8 @@ dark_theme() {
   sed -i "s/let g:material_theme_style = 'lighter'/let g:material_theme_style = 'darker'/g" /home/tim/.config/nvim/colors.vim
   sed -i "s/gtk-application-prefer-dark-theme=0/gtk-application-prefer-dark-theme=1/g" /home/tim/.config/gtk-3.0/settings.ini
   sed -i "s/gtk-application-prefer-dark-theme=0/gtk-application-prefer-dark-theme=1/g" /home/tim/.config/gtk-4.0/settings.ini
-  sed -i "s/\"workbench.colorTheme\": \"Material Theme Lighter\",/\"workbench.colorTheme\": \"Horizon\",/g" /home/tim/.config/Code/User/settings.json
-  sed -i "s/\"workbench.colorTheme\": \"Material Theme Lighter\",/\"workbench.colorTheme\": \"Horizon\",/g" /home/tim/.var/app/com.visualstudio.code/config/Code/User/settings.json
+  sed -i "s/\"workbench.colorTheme\": \"Material Theme Lighter\",/\"workbench.colorTheme\": \"Material Theme Ocean\",/g" /home/tim/.config/Code/User/settings.json
+  sed -i "s/\"workbench.colorTheme\": \"Material Theme Lighter\",/\"workbench.colorTheme\": \"Material Theme Ocean\",/g" /home/tim/.var/app/com.visualstudio.code/config/Code/User/settings.json
   [[ -f /home/tim/.config/spicetify/config-xpui.ini ]] && sed -i "s/color_scheme            = light/color_scheme            = dark/g" /home/tim/.config/spicetify/config-xpui.ini && spicetify -qn apply enable-devtool
 }
 
@@ -13,8 +13,8 @@ light_theme() {
   sed -i "s/let g:material_theme_style = 'darker'/let g:material_theme_style = 'lighter'/g" /home/tim/.config/nvim/colors.vim
   sed -i "s/gtk-application-prefer-dark-theme=1/gtk-application-prefer-dark-theme=0/g" /home/tim/.config/gtk-3.0/settings.ini
   sed -i "s/gtk-application-prefer-dark-theme=1/gtk-application-prefer-dark-theme=0/g" /home/tim/.config/gtk-4.0/settings.ini
-  sed -i "s/\"workbench.colorTheme\": \"Horizon\",/\"workbench.colorTheme\": \"Material Theme Lighter\",/g" /home/tim/.config/Code/User/settings.json
-  sed -i "s/\"workbench.colorTheme\": \"Horizon\",/\"workbench.colorTheme\": \"Material Theme Lighter\",/g" /home/tim/.var/app/com.visualstudio.code/config/Code/User/settings.json
+  sed -i "s/\"workbench.colorTheme\": \"Material Theme Ocean\",/\"workbench.colorTheme\": \"Material Theme Lighter\",/g" /home/tim/.config/Code/User/settings.json
+  sed -i "s/\"workbench.colorTheme\": \"Material Theme Ocean\",/\"workbench.colorTheme\": \"Material Theme Lighter\",/g" /home/tim/.var/app/com.visualstudio.code/config/Code/User/settings.json
   [[ -f /home/tim/.config/spicetify/config-xpui.ini ]] && sed -i "s/color_scheme            = dark/color_scheme            = light/g" /home/tim/.config/spicetify/config-xpui.ini && spicetify -qn apply enable-devtool
 }
 
