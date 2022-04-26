391f11a7e7ccf6d0c2ce76540e489d0637e03469
TimFB <56914732+TibiIius@users.noreply.github.com>
2022-04-26 20:26:12 +0200

    [scripts] remove gtk 4 from change-theme

--- scripts/change-theme.sh
+++ scripts/change-theme.sh
@@ -3,7 +3,6 @@
 dark_theme() {
   sed -i "s/let g:material_theme_style = 'lighter'/let g:material_theme_style = 'darker'/g" /home/tim/.config/nvim/colors.vim
   sed -i "s/gtk-application-prefer-dark-theme=0/gtk-application-prefer-dark-theme=1/g" /home/tim/.config/gtk-3.0/settings.ini
-  sed -i "s/gtk-application-prefer-dark-theme=0/gtk-application-prefer-dark-theme=1/g" /home/tim/.config/gtk-4.0/settings.ini
   sed -i "s/\"workbench.colorTheme\": \"Atom One Light\",/\"workbench.colorTheme\": \"One Dark Pro\",/g" /home/tim/.config/Code/User/settings.json
   sed -i "s/\"workbench.colorTheme\": \"Atom One Light\",/\"workbench.colorTheme\": \"One Dark Pro\",/g" /home/tim/.var/app/com.visualstudio.code/config/Code/User/settings.json
   [[ -f /home/tim/.config/spicetify/config-xpui.ini ]] && sed -i "s/color_scheme            = light/color_scheme            = dark/g" /home/tim/.config/spicetify/config-xpui.ini && spicetify -qn apply enable-devtool
@@ -12,7 +11,6 @@ dark_theme() {
 light_theme() {
   sed -i "s/let g:material_theme_style = 'darker'/let g:material_theme_style = 'lighter'/g" /home/tim/.config/nvim/colors.vim
   sed -i "s/gtk-application-prefer-dark-theme=1/gtk-application-prefer-dark-theme=0/g" /home/tim/.config/gtk-3.0/settings.ini
-  sed -i "s/gtk-application-prefer-dark-theme=1/gtk-application-prefer-dark-theme=0/g" /home/tim/.config/gtk-4.0/settings.ini
   sed -i "s/\"workbench.colorTheme\": \"One Dark Pro\",/\"workbench.colorTheme\": \"Atom One Light\",/g" /home/tim/.config/Code/User/settings.json
   sed -i "s/\"workbench.colorTheme\": \"One Dark Pro\",/\"workbench.colorTheme\": \"Atom One Light\",/g" /home/tim/.var/app/com.visualstudio.code/config/Code/User/settings.json
   [[ -f /home/tim/.config/spicetify/config-xpui.ini ]] && sed -i "s/color_scheme            = dark/color_scheme            = light/g" /home/tim/.config/spicetify/config-xpui.ini && spicetify -qn apply enable-devtool
