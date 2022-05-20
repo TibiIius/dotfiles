6fdbf327a231a5e1b6d19c6151b9d8222a3b208d
TimFB <56914732+TibiIius@users.noreply.github.com>
2022-05-20 02:26:28 +0200

    [scripts] fix spicetify command in change-theme.sh

--- scripts/change-theme.sh
+++ scripts/change-theme.sh
@@ -5,7 +5,7 @@ dark_theme() {
   sed -i "s/gtk-application-prefer-dark-theme=0/gtk-application-prefer-dark-theme=1/g" /home/tim/.config/gtk-3.0/settings.ini
   sed -i "s/\"workbench.colorTheme\": \"Atom One Light\",/\"workbench.colorTheme\": \"One Dark Pro\",/g" /home/tim/.config/Code/User/settings.json
   sed -i "s/\"workbench.colorTheme\": \"Atom One Light\",/\"workbench.colorTheme\": \"One Dark Pro\",/g" /home/tim/.var/app/com.visualstudio.code/config/Code/User/settings.json
-  [[ -f /home/tim/.config/spicetify/config-xpui.ini ]] && sed -i "s/color_scheme            = light/color_scheme            = dark/g" /home/tim/.config/spicetify/config-xpui.ini && spicetify -qn apply enable-devtool
+  [[ -f /home/tim/.config/spicetify/config-xpui.ini ]] && sed -i "s/color_scheme            = light/color_scheme            = dark/g" /home/tim/.config/spicetify/config-xpui.ini && spicetify -qn apply enable-devtools
 }
 
 light_theme() {
@@ -13,7 +13,7 @@ light_theme() {
   sed -i "s/gtk-application-prefer-dark-theme=1/gtk-application-prefer-dark-theme=0/g" /home/tim/.config/gtk-3.0/settings.ini
   sed -i "s/\"workbench.colorTheme\": \"One Dark Pro\",/\"workbench.colorTheme\": \"Atom One Light\",/g" /home/tim/.config/Code/User/settings.json
   sed -i "s/\"workbench.colorTheme\": \"One Dark Pro\",/\"workbench.colorTheme\": \"Atom One Light\",/g" /home/tim/.var/app/com.visualstudio.code/config/Code/User/settings.json
-  [[ -f /home/tim/.config/spicetify/config-xpui.ini ]] && sed -i "s/color_scheme            = dark/color_scheme            = light/g" /home/tim/.config/spicetify/config-xpui.ini && spicetify -qn apply enable-devtool
+  [[ -f /home/tim/.config/spicetify/config-xpui.ini ]] && sed -i "s/color_scheme            = dark/color_scheme            = light/g" /home/tim/.config/spicetify/config-xpui.ini && spicetify -qn apply enable-devtools
 }
 
 check_files() {
