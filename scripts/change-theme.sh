392716721e04b5f2f2f46f1ee7d55ee2070fa923
TimFB <56914732+TibiIius@users.noreply.github.com>
2022-08-01 00:57:02 +0200

    [scripts] material darker -> material palenight

--- scripts/change-theme.sh
+++ scripts/change-theme.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 dark_theme() {
-  sed -i "s/let g:material_theme_style = 'lighter'/let g:material_theme_style = 'darker'/g" /home/tim/.config/nvim/colors.vim
+  sed -i "s/let g:material_theme_style = 'lighter'/let g:material_theme_style = 'palenight'/g" /home/tim/.config/nvim/colors.vim
   sed -i "s/gtk-application-prefer-dark-theme=0/gtk-application-prefer-dark-theme=1/g" /home/tim/.config/gtk-3.0/settings.ini
-  sed -i "s/\"workbench.colorTheme\": \"Material Theme Lighter High Contrast\",/\"workbench.colorTheme\": \"Material Theme Darker High Contrast\",/g" /home/tim/.config/Code/User/settings.json
-  sed -i "s/\"workbench.colorTheme\": \"Material Theme Lighter High Contrast\",/\"workbench.colorTheme\": \"Material Theme Darker High Contrast\",/g" /home/tim/.var/app/com.visualstudio.code/config/Code/User/settings.json
-  sed -i 's/solarized-light/solarized-dark/g' ./.hyper.js
+  sed -i "s/\"workbench.colorTheme\": \"Material Theme Lighter High Contrast\"*/\"workbench.colorTheme\": \"Material Theme Palenight High Contrast\"/g" /home/tim/.config/Code/User/settings.json
+  sed -i "s/\"workbench.colorTheme\": \"Material Theme Lighter High Contrast\"*/\"workbench.colorTheme\": \"Material Theme Palenight High Contrast\"/g" /home/tim/.var/app/com.visualstudio.code/config/Code/User/settings.json
+  sed -i 's/one-light/one-dark/g' ./.hyper.js
   [[ -f /home/tim/.config/spicetify/config-xpui.ini ]] && sed -i "s/color_scheme            = light/color_scheme            = dark/g" /home/tim/.config/spicetify/config-xpui.ini && spicetify -qn apply enable-devtools
 }
 
 light_theme() {
-  sed -i "s/let g:material_theme_style = 'darker'/let g:material_theme_style = 'lighter'/g" /home/tim/.config/nvim/colors.vim
+  sed -i "s/let g:material_theme_style = 'palenight'/let g:material_theme_style = 'lighter'/g" /home/tim/.config/nvim/colors.vim
   sed -i "s/gtk-application-prefer-dark-theme=1/gtk-application-prefer-dark-theme=0/g" /home/tim/.config/gtk-3.0/settings.ini
-  sed -i "s/\"workbench.colorTheme\": \"Material Theme Darker High Contrast\",/\"workbench.colorTheme\": \"Material Theme Lighter High Contrast\",/g" /home/tim/.config/Code/User/settings.json
-  sed -i "s/\"workbench.colorTheme\": \"Material Theme Darker High Contrast\",/\"workbench.colorTheme\": \"Material Theme Lighter High Contrast\",/g" /home/tim/.var/app/com.visualstudio.code/config/Code/User/settings.json
-  sed -i 's/solarized-dark/solarized-light/g' ./.hyper.js
+  sed -i "s/\"workbench.colorTheme\": \"Material Theme Palenight High Contrast\"*/\"workbench.colorTheme\": \"Material Theme Lighter High Contrast\"/g" /home/tim/.config/Code/User/settings.json
+  sed -i "s/\"workbench.colorTheme\": \"Material Theme Palenight High Contrast\"*/\"workbench.colorTheme\": \"Material Theme Lighter High Contrast\"/g" /home/tim/.var/app/com.visualstudio.code/config/Code/User/settings.json
+  sed -i 's/one-dark/one-light/g' ./.hyper.js
   [[ -f /home/tim/.config/spicetify/config-xpui.ini ]] && sed -i "s/color_scheme            = dark/color_scheme            = light/g" /home/tim/.config/spicetify/config-xpui.ini && spicetify -qn apply enable-devtools
 }
 
