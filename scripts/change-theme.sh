c9e82ac680db537f09c924781296c3359ea761e1
TimFB <56914732+TibiIius@users.noreply.github.com>
2022-10-04 00:01:45 +0200

    [change-theme.sh] fix nvim theme

--- scripts/change-theme.sh
+++ scripts/change-theme.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 dark_theme() {
-  sed -i "s/let g:material_theme_style = 'lighter'/let g:material_theme_style = 'palenight'/g" /home/tim/.config/nvim/colors.vim
+  sed -i "s/let g:material_style = 'lighter'/let g:material_style = 'palenight'/g" /home/tim/.config/nvim/colors.vim
   sed -i "s/gtk-application-prefer-dark-theme=0/gtk-application-prefer-dark-theme=1/g" /home/tim/.config/gtk-3.0/settings.ini
   sed -i "s/\"workbench.colorTheme\": \"Material Theme Lighter High Contrast\"*/\"workbench.colorTheme\": \"Material Theme Palenight High Contrast\"/g" /home/tim/.config/Code/User/settings.json
   sed -i "s/\"workbench.colorTheme\": \"Material Theme Lighter High Contrast\"*/\"workbench.colorTheme\": \"Material Theme Palenight High Contrast\"/g" /home/tim/.var/app/com.visualstudio.code/config/Code/User/settings.json
@@ -11,7 +11,7 @@ dark_theme() {
 }
 
 light_theme() {
-  sed -i "s/let g:material_theme_style = 'palenight'/let g:material_theme_style = 'lighter'/g" /home/tim/.config/nvim/colors.vim
+  sed -i "s/let g:material_style = 'palenight'/let g:material_style = 'lighter'/g" /home/tim/.config/nvim/colors.vim
   sed -i "s/gtk-application-prefer-dark-theme=1/gtk-application-prefer-dark-theme=0/g" /home/tim/.config/gtk-3.0/settings.ini
   sed -i "s/\"workbench.colorTheme\": \"Material Theme Palenight High Contrast\"*/\"workbench.colorTheme\": \"Material Theme Lighter High Contrast\"/g" /home/tim/.config/Code/User/settings.json
   sed -i "s/\"workbench.colorTheme\": \"Material Theme Palenight High Contrast\"*/\"workbench.colorTheme\": \"Material Theme Lighter High Contrast\"/g" /home/tim/.var/app/com.visualstudio.code/config/Code/User/settings.json
