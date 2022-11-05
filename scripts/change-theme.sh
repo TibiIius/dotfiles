096169f9e1a7d3a61c496aa721d44e95bc0ced37
TimFB <56914732+TibiIius@users.noreply.github.com>
2022-11-05 01:49:36 +0100

    [scripts/change-theme] remove spicetify

--- scripts/change-theme.sh
+++ scripts/change-theme.sh
@@ -8,7 +8,6 @@ dark_theme() {
   sed -i "s/gtk-application-prefer-dark-theme=0/gtk-application-prefer-dark-theme=1/g" /home/tim/.config/gtk-3.0/settings.ini
   sed -i "s/\"workbench.colorTheme\": \"Material Theme Lighter High Contrast\"*/\"workbench.colorTheme\": \"Material Theme Palenight High Contrast\"/g" /home/tim/.config/Code/User/settings.json
   sed -i 's/OneHalfLight/OneHalfDark/g' $HOME/.config/bat/config
-  [[ -f /home/tim/.config/spicetify/config-xpui.ini ]] && sed -i "s/color_scheme            = light/color_scheme            = dark/g" /home/tim/.config/spicetify/config-xpui.ini && spicetify -qn apply enable-devtools
   ln -sf $HOME/.config/kitty/themes-github/DarkOneNuanced.conf $HOME/.config/kitty/current-theme.conf
 }
 
@@ -17,7 +16,6 @@ light_theme() {
   sed -i "s/gtk-application-prefer-dark-theme=1/gtk-application-prefer-dark-theme=0/g" /home/tim/.config/gtk-3.0/settings.ini
   sed -i "s/\"workbench.colorTheme\": \"Material Theme Palenight High Contrast\"*/\"workbench.colorTheme\": \"Material Theme Lighter High Contrast\"/g" /home/tim/.config/Code/User/settings.json
   sed -i 's/OneHalfDark/OneHalfLight/g' $HOME/.config/bat/config
-  [[ -f /home/tim/.config/spicetify/config-xpui.ini ]] && sed -i "s/color_scheme            = dark/color_scheme            = light/g" /home/tim/.config/spicetify/config-xpui.ini && spicetify -qn apply enable-devtools
   ln -sf $HOME/.config/kitty/themes-github/AtomOneLight.conf $HOME/.config/kitty/current-theme.conf
 }
 
