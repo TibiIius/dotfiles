b2748078248b0bc5d50b32bf310824ac32f204c2
Tim Friedrich Brüggemann <oss@timfb.dev>
2025-09-03 17:52:17 +0200

    fix: Add missing tmux bindings

--- root/dot_wezterm.lua
+++ root/dot_wezterm.lua
@@ -39,14 +39,14 @@ config = {
 			-- This means that this binding will be invoked when you press the leader
 			-- (CTRL + A), quickly followed by quotes (").
 			mods = "LEADER|SHIFT",
-			action = wezterm.action.SplitHorizontal({
+			action = wezterm.action.SplitVertical({
 				domain = "CurrentPaneDomain",
 			}),
 		},
 		{
 			key = "%",
 			mods = "LEADER",
-			action = wezterm.action.SplitVertical({
+			action = wezterm.action.SplitHorizontal({
 				domain = "CurrentPaneDomain",
 			}),
 		},
@@ -54,6 +54,26 @@ config = {
 			key = "[",
 			mods = "LEADER",
 			action = wezterm.action.ActivateCopyMode,
+		},
+		{
+			key = "h",
+			mods = "LEADER",
+			action = wezterm.action.ActivatePaneDirection("Left"),
+		},
+		{
+			key = "j",
+			mods = "LEADER",
+			action = wezterm.action.ActivatePaneDirection("Down"),
+		},
+		{
+			key = "k",
+			mods = "LEADER",
+			action = wezterm.action.ActivatePaneDirection("Up"),
+		},
+		{
+			key = "l",
+			mods = "LEADER",
+			action = wezterm.action.ActivatePaneDirection("Right"),
 		},
 	},
 	font = wezterm.font("Liga SFMono Nerd Font", {
